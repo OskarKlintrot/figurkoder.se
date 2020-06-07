aa7ec28233e45b728753a351e764632dab7ec1d9
Oskar Klintrot <oskar.klintrot@gmail.com>
2020-06-07 12:46:20 +0200

    feat: Show current flash card and trigger end of game event when out of flash cards

--- blazor/src/Figurkoder/Figurkoder.Domain/GameEngine.cs
+++ blazor/src/Figurkoder/Figurkoder.Domain/GameEngine.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Timers;
 
 namespace Figurkoder.Domain
@@ -7,16 +8,23 @@ namespace Figurkoder.Domain
     public sealed class GameEngine
     {
         private readonly Timer _timer;
+        private int _counter;
+        private KeyValuePair<string, string>[] _flashCards = Array.Empty<KeyValuePair<string, string>>();
 
         /// <summary>
         /// Shows the current flash card
         /// </summary>
-        public event EventHandler<EventArgs>? Current;
+        public event EventHandler<CurrentEventArgs>? Current;
 
         /// <summary>
         /// Triggers when the game starts
         /// </summary>
         public event EventHandler<EventArgs>? GameStart;
+
+        /// <summary>
+        /// Triggers when the game has finished
+        /// </summary>
+        public event EventHandler<EventArgs>? GameFinished;
 
         public GameEngine()
         {
@@ -24,34 +32,82 @@ namespace Figurkoder.Domain
             _timer.Elapsed += TimerElapsed;
         }
 
-        public void Start(Game newGame)
+        public void Start(Game game)
         {
-            _timer.Interval = newGame.FlashTime.TotalMilliseconds;
-
-            _timer.Start();
-
-            OnStart(EventArgs.Empty);
-            OnShowNext(EventArgs.Empty);
+            OnStart(game);
+            UpdateCurrent();
         }
 
         private void TimerElapsed(object sender, ElapsedEventArgs e)
         {
-            OnShowNext(e);
+            UpdateCurrent();
         }
 
-        private void OnStart(EventArgs e)
+        private void OnStart(Game game)
         {
-            GameStart?.Invoke(this, e);
+            _counter = 0;
+
+            _timer.Interval = game.FlashTime.TotalMilliseconds;
+
+            _flashCards = game.FlashCards;
+
+            if (_flashCards.Length == 0)
+            {
+                throw new ArgumentException("Missing flash cards!", nameof(game));
+            }
+
+            GameStart?.Invoke(this, EventArgs.Empty);
+
+            _timer.Start();
         }
 
-        private void OnShowNext(EventArgs e)
+        private void OnFinished(EventArgs e)
         {
+            _timer.Stop();
+
+            GameFinished?.Invoke(this, e);
+        }
+
+        private void UpdateCurrent()
+        {
+            if (_counter >= _flashCards.Length)
+            {
+                OnFinished(EventArgs.Empty);
+
+                return;
+            }
+
+            _counter++;
+
+            var e = new CurrentEventArgs(_counter, _flashCards[_counter-1]);
+
             Current?.Invoke(this, e);
+        }
+    }
+
+    public sealed class CurrentEventArgs : EventArgs
+    {
+        public int Count { get; }
+        public int TotalCount { get; }
+        public KeyValuePair<string, string> Current { get; }
+
+        public CurrentEventArgs(int count, KeyValuePair<string, string> current)
+        {
+            Count = count;
+            Current = current;
         }
     }
 
     public sealed class Game
     {
-        public TimeSpan FlashTime { get; set; } = TimeSpan.FromSeconds(6);
+        public Game(TimeSpan flashTime, KeyValuePair<string, string>[] flashCards)
+        {
+            FlashTime = flashTime;
+            FlashCards = flashCards;
+        }
+
+        public TimeSpan FlashTime { get; }
+
+        public KeyValuePair<string, string>[] FlashCards { get; }
     }
 }
--- blazor/src/Figurkoder/Tests/Figurkoder.UnitTests/Domain/GameEngineFacts.cs
+++ blazor/src/Figurkoder/Tests/Figurkoder.UnitTests/Domain/GameEngineFacts.cs
@@ -1,5 +1,6 @@
 using Figurkoder.Domain;
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Threading;
 using Xunit;
@@ -12,20 +13,40 @@ namespace Figurkoder.UnitTests.Domain
         public void Start_GameStarts_CurrentEventTriggers()
         {
             // Arrange
-            var game = new GameEngine();
+            var gameEngine = new GameEngine();
             var currentReceived = new AutoResetEvent(false);
-            game.Current += CurrentHandler;
+            gameEngine.Current += CurrentHandler;
 
-            void CurrentHandler(object _, EventArgs e)
+            void CurrentHandler(object? _, EventArgs e)
             {
                 currentReceived.Set();
             }
 
             // Act
-            game.Start(new Game { FlashTime = TimeSpan.FromMilliseconds(100) });
+            gameEngine.Start(new Game(TimeSpan.FromMilliseconds(100), new [] { KeyValuePair.Create("Foo", "Bar") }));
 
             // Assert
             Assert.True(currentReceived.WaitOne(TimeSpan.FromMilliseconds(10)));
+        }
+
+        [Fact]
+        public void Start_GameEnds_GameFinishedEventTriggers()
+        {
+            // Arrange
+            var gameEngine = new GameEngine();
+            var finishedReceived = new AutoResetEvent(false);
+            gameEngine.GameFinished += GameFinishedHandler;
+
+            void GameFinishedHandler(object? _, EventArgs e)
+            {
+                finishedReceived.Set();
+            }
+
+            // Act
+            gameEngine.Start(new Game(TimeSpan.FromMilliseconds(50), new[] { KeyValuePair.Create("Foo", "Bar") }));
+
+            // Assert
+            Assert.True(finishedReceived.WaitOne(TimeSpan.FromMilliseconds(75)));
         }
 
         [Fact]
@@ -33,17 +54,17 @@ namespace Figurkoder.UnitTests.Domain
         {
             // Arrange
             var currentCounter = 0;
-            var game = new GameEngine();
+            var gameEngine = new GameEngine();
             var sw = new Stopwatch();
             var currentReceived = new AutoResetEvent(false);
-            game.GameStart += GameStartHandler;
-            game.Current += CurrentHandler;
+            gameEngine.GameStart += GameStartHandler;
+            gameEngine.Current += CurrentHandler;
 
-            void GameStartHandler(object _, EventArgs e)
+            void GameStartHandler(object? _, EventArgs e)
             {
                 sw.Start();
             }
-            void CurrentHandler(object _, EventArgs e)
+            void CurrentHandler(object? _, EventArgs e)
             {
                 currentCounter++;
 
@@ -55,11 +76,54 @@ namespace Figurkoder.UnitTests.Domain
             }
 
             // Act
-            game.Start(new Game { FlashTime = TimeSpan.FromMilliseconds(100) });
+            gameEngine.Start(new Game(TimeSpan.FromMilliseconds(100), new[] { KeyValuePair.Create("Foo", "Bar"), KeyValuePair.Create("Foo", "Bar") }));
 
             // Assert
             currentReceived.WaitOne(TimeSpan.FromMilliseconds(115));
-            Assert.InRange(sw.Elapsed, TimeSpan.FromMilliseconds(85), TimeSpan.FromMilliseconds(115)); // Allow ±15ms
+            Assert.InRange(sw.Elapsed, TimeSpan.FromMilliseconds(80), TimeSpan.FromMilliseconds(120)); // Allow ±20ms
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(4)]
+        [InlineData(5)]
+        [InlineData(6)]
+        [InlineData(7)]
+        [InlineData(8)]
+        [InlineData(9)]
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "xUnit1026:Theory methods should use all of their parameters", Justification = "Run test 10 times to make sure order is respected")]
+        public void Start_TwoFlashCard_CurrentEventTriggersTwoTimes(int _)
+        {
+            // Arrange
+            var gameEngine = new GameEngine();
+            CurrentEventArgs? currentEventArgs = null;
+            var currentReceived = new AutoResetEvent(false);
+            gameEngine.Current += CurrentHandler;
+
+            void CurrentHandler(object? _, CurrentEventArgs e)
+            {
+                currentEventArgs = e;
+                currentReceived.Set();
+            }
+
+            // Act
+            gameEngine.Start(new Game(TimeSpan.FromMilliseconds(10), new[] {
+                KeyValuePair.Create("FirstKey", "FirstValue"),
+                KeyValuePair.Create("SecondKey", "SecondValue") }));
+
+            // Assert
+            Assert.True(currentReceived.WaitOne(TimeSpan.FromMilliseconds(30)));
+            Assert.Equal(1, currentEventArgs?.Count);
+            Assert.Equal("FirstKey", currentEventArgs?.Current.Key);
+            Assert.Equal("FirstValue", currentEventArgs?.Current.Value);
+
+            Assert.True(currentReceived.WaitOne(TimeSpan.FromMilliseconds(30)));
+            Assert.Equal(2, currentEventArgs?.Count);
+            Assert.Equal("SecondKey", currentEventArgs?.Current.Key);
+            Assert.Equal("SecondValue", currentEventArgs?.Current.Value);
         }
     }
 }

5040cd0681e1eaae1233955195acfcc5d8560927
Oskar Klintrot <oskar.klintrot@gmail.com>
2021-09-17 10:26:00 +0200

    refactor(upgrade): Use SupplyParameterFromQuery instead of home brew solution

--- blazor/src/Figurkoder/Figurkoder.WebApp/Program.cs
+++ blazor/src/Figurkoder/Figurkoder.WebApp/Program.cs
@@ -1,25 +1,16 @@
+using Figurkoder.WebApp;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Net.Http;
-using System.Threading.Tasks;
 
-namespace Figurkoder.WebApp
-{
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("#app");
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
 
-            builder.Services
-                .AddDomain()
-                .AddApplication()
-                .AddInfrastructure()
-                .AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services
+    .AddDomain()
+    .AddApplication()
+    .AddInfrastructure()
+    .AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-            await builder.Build().RunAsync();
-        }
-    }
-}
+await builder.Build().RunAsync();

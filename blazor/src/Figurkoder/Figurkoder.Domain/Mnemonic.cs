276402cf7a469877064496b2a94e19f032786688
Oskar Klintrot <oskar.klintrot@gmail.com>
2021-09-18 19:19:14 +0200

    refactor(game): Move logic for selecting pairs up one component

--- blazor/src/Figurkoder/Figurkoder.Domain/Mnemonic.cs
+++ blazor/src/Figurkoder/Figurkoder.Domain/Mnemonic.cs
@@ -3,18 +3,8 @@ using System.Linq;
 
 namespace Figurkoder.Domain
 {
-    public class Mnemonic
+    public record Mnemonic(string Title, string Description, Flashcard[] Pairs)
     {
-        public string Title { get; }
-        public string Description { get; }
-        public Flashcard[] Pairs { get; }
         public bool Numerical => Pairs.All(y => int.TryParse(y.Key, out var _));
-
-        public Mnemonic(string title, string description, Flashcard[] pairs)
-        {
-            Title = title;
-            Description = description;
-            Pairs = pairs;
-        }
     }
 }
